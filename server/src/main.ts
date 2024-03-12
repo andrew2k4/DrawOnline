babf86fbf8182bf859faaa958a750e67ed57a8b3
spocky <nguemnangandrew@gmail.com>
2024-03-12 18:01:35 +0100

    init docker

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import Redis from "ioredis";
-import { channel } from "diagnostics_channel";
+import closeWithGrace from "close-with-grace";
+import { randomUUID } from "crypto";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const REDIS_CACHE_URL = process.env.UPSTASH_REDIS_REST_URL;
 
 const CONNECTION_COUNT_KEY = "chat:connection-count";
 const CONNECTION_COUNT_UPDATE_CHANNEL = "chat:connection-count-update";
+const NEW_MESSAGE_CHANNEL = "chat:new-message";
 
 if (!REDIS_CACHE_URL) {
   console.error("REDIS CACHE IS MISSING");
@@ -23,6 +25,8 @@ if (!REDIS_CACHE_URL) {
 
 const publisher = new Redis(REDIS_CACHE_URL);
 const subscriber = new Redis(REDIS_CACHE_URL);
+
+let connectedClients = 0;
 
 async function buildServer() {
   const app = express();
@@ -44,13 +48,26 @@ async function buildServer() {
 
   io.on("connection", async (socket) => {
     console.log("Client connected");
-
     const incResult = await publisher.incr(CONNECTION_COUNT_KEY);
+    connectedClients++;
 
     await publisher.publish(CONNECTION_COUNT_UPDATE_CHANNEL, String(incResult));
 
+    socket.on(NEW_MESSAGE_CHANNEL, async (payload) => {
+      const message = payload.message;
+
+      if (!message) {
+        return;
+      }
+
+      console.log("rest", message);
+      await publisher.publish(NEW_MESSAGE_CHANNEL, message.toString());
+    });
+
     socket.on("disconnect", async () => {
       console.log("Client disconnected");
+
+      connectedClients--;
       const decrResult = await publisher.decr(CONNECTION_COUNT_KEY);
 
       await publisher.publish(
@@ -73,12 +90,34 @@ async function buildServer() {
     );
   });
 
+  subscriber.subscribe(NEW_MESSAGE_CHANNEL, (err, count) => {
+    if (err) {
+      console.error(
+        "Error subscribe" + { CONNECTION_COUNT_UPDATE_CHANNEL } + err
+      );
+      return;
+    }
+
+    console.log(
+      `${count} clients subscribes to ${NEW_MESSAGE_CHANNEL} channel`
+    );
+  });
+
   subscriber.on("message", (channel, text) => {
-    if ((channel = "CONNECTION_COUNT_UPDATE_CHANNEL")) {
+    if (channel === CONNECTION_COUNT_UPDATE_CHANNEL) {
       io.emit(CONNECTION_COUNT_UPDATE_CHANNEL, {
         count: text,
       });
+      return;
+    }
 
+    if (channel === NEW_MESSAGE_CHANNEL) {
+      io.emit(NEW_MESSAGE_CHANNEL, {
+        message: text,
+        id: randomUUID(),
+        createdAt: new Date(),
+        port: PORT,
+      });
       return;
     }
   });
@@ -90,12 +129,30 @@ async function main() {
   const app = await buildServer();
 
   try {
-    app.listen(PORT, HOST, () =>
-      console.log(`Server running at ${HOST}:${PORT}`)
-    );
+    await app.listen({
+      port: PORT,
+      host: HOST,
+    });
+
+    closeWithGrace({ delay: 2000 }, async ({ signal, err }) => {
+      if (connectedClients > 0) {
+        const currentCount = parseInt(
+          (await publisher.get(CONNECTION_COUNT_KEY)) || "0",
+          10
+        );
+
+        const newCount = Math.max(currentCount - connectedClients, 0);
+
+        await publisher.set(CONNECTION_COUNT_KEY, newCount);
+      }
+
+      await app.close();
+    });
+
+    console.log(`Server started at http://${HOST}:${PORT}`);
   } catch (e) {
     console.error(e);
-    return process.exit(1);
+    process.exit(1);
   }
 }
 
